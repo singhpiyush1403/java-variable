fd9a2d27dfc98072cb49e6c25435d380aa6e8658
PIYUSH SINGH <singhpiyush1403@gmail.com>
2024-11-17 16:02:48 +0530

    Add files via upload

---
+++ java variable.java
@@ -0,0 +1,21 @@
+public class Main {
+	public static void main(String args []) {
+		boolean isPassed = true;
+		int count = 12;
+		System.out.println(isPassed);
+		System.out.println(count);
+		
+		float  pi= 3.12f;
+		float xi = 3.1f;
+		System.out.println(pi);
+		System.out.println(xi);
+		
+		char myLetter = 'A';
+		System.out.println(myLetter);
+		
+		int age = 200;
+		int newage = (byte)age;
+		System.out.println(newage);
+		
+	}
+}
